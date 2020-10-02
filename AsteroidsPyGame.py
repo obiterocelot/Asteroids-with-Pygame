4fd33ab2e44f0a9f68e963d08b0ab3e204086acf
Sarah Kelly <sarahblairkelly@gmail.com>
2020-10-02 16:22:22 +0200

    Update AsteroidsPyGame.py
    Working playable SHIP and rendered BULLET v1

--- AsteroidsPyGame.py
+++ AsteroidsPyGame.py
@@ -1,22 +1,22 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
+# Name:        AsteroidsPyGame.py
+# Purpose:     education
 # Author:      sarah
-#
 # Created:     15/09/2020
 # Copyright:   (c) sarah 2020
-# Licence:     <your licence>
+# Licence:     coding help on Vectors: https://stackoverflow.com/questions/48856922/pygame-how-to-make-sprite-move-in-the-direction-its-facing-with-vectors
 #-------------------------------------------------------------------------------
 
 import pygame
 import math
 
-from pygame.locals import (RLEACCEL, K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, KEYUP, QUIT)
+from pygame.locals import (RLEACCEL, K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, K_SPACE, KEYDOWN, KEYUP, QUIT)
 from pygame.math import Vector2
 
 screen_width = 800
 screen_height = 600 #placeholder screensize
+
+max_speed = 10 #this is the max speed for all sprites - including bullets
 
 class Player(pygame.sprite.Sprite):
     """this class defines the attributes of your controlable sprite"""
@@ -46,11 +46,12 @@ class Player(pygame.sprite.Sprite):
             self.speed += self.accel
 
         #setting a max speed and adjusting the Vector to that max speed
-        if self.speed.length() > 10:
-            self.speed.scale_to_length(10) #scales the direction down to the speed so it's not expontentially if you're going at an angle
+        if self.speed.length() > max_speed:
+            self.speed.scale_to_length(max_speed) #scales the direction down to the speed so it's not expontentially if you're going at an angle
 
         self.pos += self.speed  #updates the new player position to a factor of the given speed
         self.rect.center = self.pos     #updates the surface drawing to the new player position
+
 
     def rotate(self):
         """providing the rotation of the acceleration Vector"""
@@ -64,6 +65,11 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.transform.rotate(self.surf_copy, -self.angle)
         self.rect = self.surf.get_rect(center=self.rect.center)
 
+    def shoot(self):
+        bullet = Bullet(self.pos.x, self.pos.y)
+        all_sprites.add(bullet)
+        bullets_list.add(bullet)
+
     def screen_wrap(self):
         """rules for player wrapping around screen (each sprite is a little different)"""
         if self.pos.x > screen_width:
@@ -75,6 +81,23 @@ class Player(pygame.sprite.Sprite):
         if self.pos.y > screen_height:
             self.pos.y = 0
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Bullet, self).__init__()
+        self.surf = pygame.Surface((5, 5))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+        self.speed = Vector2(0, -max_speed)
+        self.pos = Vector2(x, y)
+
+    def update(self):
+        self.pos += self.speed
+        self.rect.center = self.pos
+        if self.pos.x > screen_width or self.pos.x < 0:
+            self.kill()
+        if self.pos.y > screen_height or self.pos.y <= 0:
+            self.kill()
+
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -82,6 +105,9 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 player = Player()
+bullets_list = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 running = True
 while running:
@@ -93,14 +119,22 @@ while running:
         elif event.type == QUIT:
             running = False
 
+        if event.type == KEYDOWN and event.key == K_SPACE:
+            player.shoot()
+
     #player movement
     pressed_keys = pygame.key.get_pressed()
     player.screen_wrap()
     player.update(pressed_keys)
+    bullets_list.update()
 
     screen.fill((0, 0, 0)) #black
 
-    screen.blit(player.surf, player.rect)
+    for each in all_sprites:
+        screen.blit(each.surf, each.rect)
+
+    for each in bullets_list:
+        screen.blit(each.surf, each.rect)
 
     pygame.display.flip()
 
