6a877c2c21d7b50dd26d37609d8c510314ea22d7
Sarah Kelly <sarahblairkelly@gmail.com>
2020-10-13 13:22:30 +0200

    Update of Asteroids with PyGame
    Creation of Player.py and fully working with AsteroidsPyGame

--- AsteroidsPyGame.py
+++ AsteroidsPyGame.py
@@ -14,94 +14,15 @@ import copy
 
 from Asteroid import (Asteroid_BIG, Asteroid_MED, Asteroid_SML)
 import Bullet
+from Player import Player
 
 from pygame.locals import (RLEACCEL, K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, K_SPACE, KEYDOWN, KEYUP, QUIT)
 from pygame.math import Vector2
 
 screen_width = 800
 screen_height = 600 #placeholder screensize
-
 max_speed = 10 #this is the max speed for all sprites - including bullets
 ADDINGEVENT = pygame.USEREVENT +2
-
-class Player(pygame.sprite.Sprite):
-    """this class defines the attributes of your controlable sprite"""
-    def __init__(self):
-        super(Player, self).__init__()
-        self.surf = pygame.Surface((20, 25))
-        pygame.draw.polygon(self.surf, (50, 120, 180), ((10, 0), (0, 25), (20, 25)))
-        self.surf_copy = self.surf
-        self.rect = self.surf.get_rect()
-        self.accel = Vector2(0, -0.05)  # The acceleration Vector points upwards.
-        self.pos = Vector2(screen_width / 2, screen_height / 2) #starting in the middle of the screen
-        self.angle = 0
-        self.turn_speed = 0
-        self.speed = Vector2(0, 0)
-        self.gun_accel = Vector2(0, -10)    #creation of the ship's gun
-        self.immunity = True
-
-    def immune(self, list1):
-        """grants immunity for first two seconds on spawn"""
-        if self.immunity == True: #checks if there is a player in the player_list
-            pygame.time.set_timer(ADDINGEVENT, 3000)    #if not, it waits 2000 ms before it adds one to the list
-        if self.immunity == False:
-            pygame.time.set_timer(ADDINGEVENT, 0) #when one is added, it resets the counter to 0 so it doesn't keep making new players
-
-    def update(self, pressed_keys):
-        """updates the location of the sprite every frame if moved by keypress"""
-        #pressing LEFT or RIGHT will start the player turning in the given direction
-        if pressed_keys[K_LEFT]:
-            self.turn_speed = -3 #can continue to change these ints to increase/decrease turn speed
-            player.rotate()
-        if pressed_keys[K_RIGHT]:
-            self.turn_speed = 3
-            player.rotate()
-        #pressing up or down will increase or decrease the player speed by a factor of the acceleration
-        if pressed_keys[K_UP]:
-            self.speed += self.accel
-
-        #setting a max speed and adjusting the Vector to that max speed
-        if self.speed.length() > max_speed:
-            self.speed.scale_to_length(max_speed) #scales the direction down to the speed so it's not expontentially if you're going at an angle
-
-        self.pos += self.speed  #updates the new player position to a factor of the given speed
-        self.rect.center = self.pos     #updates the surface drawing to the new player position
-
-    def rotate(self):
-        """providing the rotation of the acceleration Vector"""
-        self.accel.rotate_ip(self.turn_speed) #acceleration Vector is the one moving (speed is just by what factor)
-        self.gun_accel.rotate_ip(self.turn_speed)
-        self.angle += self.turn_speed #turn to a new angle as key is held down
-        #this bit allows you to just go round and round in circles
-        if self.angle > 360:
-            self.angle -= 360
-        elif self.angle < 0:
-            self.angle += 360
-        #transforms the ship sprite
-        self.surf = pygame.transform.rotate(self.surf_copy, -self.angle)
-        self.rect = self.surf.get_rect(center=self.rect.center)
-
-    def get_gun_vec(self):
-        """pull a copy of the gun vector on command"""
-        return copy.deepcopy(self.gun_accel)    #this way the bullets wont turn as the ship turns
-
-    def shoot(self, accel):
-        """spawning your bullets"""
-        if self.immunity == False:
-            bullet = Bullet.Bullet(self.pos.x, self.pos.y, accel)
-            all_sprites.add(bullet) #and adding them to the list
-            bullets_list.add(bullet)
-
-    def screen_wrap(self):
-        """rules for player wrapping around screen (each sprite is a little different)"""
-        if self.pos.x > screen_width:
-            self.pos.x = 0
-        if self.pos.x < 0:
-            self.pos.x = screen_width
-        if self.pos.y <= 0:
-            self.pos.y = screen_height
-        if self.pos.y > screen_height:
-            self.pos.y = 0
 
 def asteroid_hit(death, accel, new_astsize, new_list, counter=0):
     """definition of what happens at an asteroid death and split. Small Asteroid is different"""
@@ -154,7 +75,7 @@ while running:
         if event.type == KEYDOWN and event.key == K_SPACE:
             """shooting the gun"""
             gun = player.get_gun_vec()
-            player.shoot(gun)
+            player.shoot(gun, all_sprites, bullets_list)
 
         if event.type == addasteroid:
             """spawning new asteroids"""
